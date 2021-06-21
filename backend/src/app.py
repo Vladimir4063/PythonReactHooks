78f7f1890eaacaf817c358dec968bb308e134a62
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-06-21 19:51:40 -0300

    Start backend

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,5 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
-from flak_cors import CORS
+from flask_cors import CORS
 
-app
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost/pythonreactdb'
+mongo = PyMongo(app)
+
+db = mongo.db.pythonreact
+
+@app.route('/users', methods = ['POST'])
+def createUser():
+    return 'receivedo'
+
+
+#Obtener todos los usuarios / get all users
+@app.route('/users', methods = ['GET'])
+def getUsers():
+    users= []
+    for doc in db.find():
+        pass
+    return jsonify()
+
+if __name__ == "__main__":
+    app.run(debug=True)
