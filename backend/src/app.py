a86b29e041fcdde23cdcb514d95392ff41e91ac1
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-06-22 00:44:40 -0300

    creat methods get and put

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,25 +1,49 @@
 from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, ObjectId
 from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost/pythonreactdb'
 mongo = PyMongo(app)
 
-db = mongo.db.pythonreact
+db = mongo.db.users
 
+#Enviar datos
 @app.route('/users', methods = ['POST'])
 def createUser():
-    return 'receivedo'
-
+    id = db.insert({
+        'name' : request.json['name'],
+        'email' : request.json['email'],
+        'password' : request.json['password']
+    })
+    return jsonify(str(ObjectId(id)))
+    
 
 #Obtener todos los usuarios / get all users
 @app.route('/users', methods = ['GET'])
 def getUsers():
-    users= []
+    users= [] #Guardo los users en una lista
     for doc in db.find():
-        pass
-    return jsonify()
+        users.append({
+            '_id': str(ObjectId(doc['_id'])),
+            'name': doc['name'],
+            'email': doc['email'],
+            'password': doc['password'] 
+        })
+    return jsonify(users)
+
+@app.route('/user/<id>', methods = ['GET'])
+def getUser(id):
+    print(id)
+    return 'received'
+
+@app.route('/users/<id>', methods = ['DELETE'])
+def deleteUser(id):
+    return 'received'
+
+@app.route('/users/<id>', methods = ['PUT'])
+def updateUser():
+    return 'received'
 
 if __name__ == "__main__":
     app.run(debug=True)
