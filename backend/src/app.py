a7de7f173ae26545f73598c855e657feb2e1b790
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-06-23 13:03:03 -0300

    conexion entre servidors (CORS)

--- backend/src/app.py
+++ backend/src/app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost/pythonreactdb'
 mongo = PyMongo(app)
 
+CORS(app) #de esta forma podemos interactuar el servidor de React, con el server de Flask=Python
+
 db = mongo.db.users
 
 #Enviar datos
