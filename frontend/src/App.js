4135407dd5b5cd9f8ed34dff1f25cf080eb43f94
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-06-27 00:33:48 -0300

    iniciando con navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import { About } from './components/About'; //Importamos el componente
 import { Users } from './components/Users';
-
+import { Navbar } from './components/Navbar';
 
 function App() {
   return ( //Instanciamos nuestra clase
     <Router>
+      <Navbar/>
       <div>
         <Switch>
           <Route path="/about" component= {About} />    
