14c3b40341d63b359d78f638c8117f850ea32056
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-06-29 00:43:16 -0300

    Interfas de formulario (sin estilizar) - Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return ( //Instanciamos nuestra clase
     <Router>
       <Navbar/>
-      <div>
+      <div className="container p-4">
         <Switch>
           <Route path="/about" component= {About} />    
           <Route path="/" component= {Users} />
